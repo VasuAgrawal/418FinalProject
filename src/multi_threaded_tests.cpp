163dd08ed26a11a4cfcff12bd04b2e88be8f877a
Vasu Agrawal <vasu_agrawal@yahoo.com>
2017-05-12 00:42:20 -0400

    Write results

--- src/multi_threaded_tests.cpp
+++ src/multi_threaded_tests.cpp
@@ -12,16 +12,16 @@ static int const kLarge = 1 << 20;
 
 void test_all() {
     // Write heavy testing
-    //std::cout << "Threads,Contention,Time (ms) (r/w)" << std::endl;
-    //for (int threads = 1; threads <= 32; threads *= 2) {
-        //for (float contention = 0; contention <= 1; contention += 1.0 / 4) {
-            //[threads, contention]() -> bool {
-              //INIT_TEST;
-              //test_n_threads_large(threads, contention - .00001);
-              //EXIT_TEST;
-            //}();
-        //}
-    //}
+    std::cout << "Threads,Contention,Time (ms) (r/w)" << std::endl;
+    for (int threads = 2; threads <= 32; threads *= 4) {
+        for (float contention = 0; contention <= 1; contention += 1.0 / 4) {
+            [threads, contention]() -> bool {
+              INIT_TEST;
+              test_n_threads_large(threads, contention - .00001);
+              EXIT_TEST;
+            }();
+        }
+    }
 
 
     // Read heavy testing
