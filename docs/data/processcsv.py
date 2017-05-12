8dbdd202c0cb8cb577ffe6fdb40cfd704eddf429
Lukas Peraza <lukasperaza@gmail.com>
2017-05-11 22:37:18 -0400

    more graphing

---
+++ docs/data/processcsv.py
@@ -0,0 +1,24 @@
+import csv
+
+def process(fname):
+    timings = dict()
+    with open(fname, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            if row[0] == 'Threads':
+                continue
+            nThreads = int(row[0])
+            contention = float(row[1])
+            ms = float(row[2])
+            if nThreads not in timings:
+                timings[nThreads] = dict()
+            timings[nThreads][contention] = ms
+
+    newname = fname[:-4] + '-proc.csv'
+    with open(newname, 'w', newline='') as newcsvfile:
+        writer = csv.writer(newcsvfile, delimiter=',')
+        row = ['Thread Count'] + sorted(timings[1])
+        writer.writerow(row)
+        for tnum in sorted(timings):
+            row = [timings[tnum][cont] for cont in sorted(timings[tnum])]
+            writer.writerow([tnum] + row)
